8dac0237be1aa40930b90714e9e9d5aedeb18ea3
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2026-01-17 14:52:25 -0500

    removed rectification

---
+++ force.py
@@ -0,0 +1,149 @@
+import numpy as np
+import pandas as pd
+import argparse
+import time
+import globals as gl
+import os
+
+def calc_md(X):
+    """
+
+    Args:
+        X: timepoints x channels data
+
+    Returns:
+
+    """
+    N, m = X.shape
+    F1 = X[0]
+    FN = X[-1] - F1  # Shift the end point
+
+    shifted_matrix = X - F1  # Shift all points
+
+    d = list()
+
+    for t in range(1, N - 1):
+        Ft = shifted_matrix[t]
+
+        # Project Ft onto the ideal straight line
+        proj = np.dot(Ft, FN) / np.dot(FN, FN) * FN
+
+        # Calculate the Euclidean distance
+        d.append(np.linalg.norm(Ft - proj))
+
+    d = np.array(d)
+    MD = d.mean()
+
+    return MD, d
+
+def main(args):
+    if args.what == 'single_trial_training':
+        path = os.path.join(gl.baseDir, gl.behavDir, f'p{args.sn}_training')
+        dat = pd.read_csv(os.path.join(path, f'efcTMS_training_{args.sn}_output.csv'))
+        ntrial = dat.shape[0]
+        out_dict = {
+            'participant_id': [],
+            'TN': [],
+            'BN': [],
+            'Chord': [],
+            'ChordSet': [],
+            'session': [],
+            'success': [],
+            'MD': [],
+            'ET': []
+        }
+        for tr in range(ntrial):
+            trial_data = pd.read_csv(os.path.join(path, 'raw_data', f'p{args.sn}_trial{tr + 1}.csv'))
+            force = trial_data.iloc[:, 1:26].to_numpy() / 1000 # divide by 1000 to get force in N
+            planTime = dat.loc[tr].Prep_dur
+            holdTime = dat.loc[tr].Match_dur
+            start_sample = int(np.floor(planTime * gl.sampling_rate))
+            end_sample = force.shape[0] - int(np.floor(holdTime * gl.sampling_rate))
+            X = force[start_sample:end_sample]
+            MD, d = calc_md(X)
+            ET = (end_sample - start_sample) / gl.sampling_rate
+            success = dat.loc[tr].Success == 'success'
+            out_dict['participant_id'].append(args.sn)
+            out_dict['TN'].append(tr + 1)
+            out_dict['BN'].append(dat.loc[tr].Block)
+            out_dict['MD'].append(MD)
+            out_dict['ET'].append(ET)
+            out_dict['Chord'].append(dat.loc[tr].Chord)
+            out_dict['ChordSet'].append('trained')
+            out_dict['session'].append('training')
+            out_dict['success'].append(success)
+        df = pd.DataFrame(out_dict)
+        df.to_csv(os.path.join(path, 'single_trial.tsv'), sep='\t', index=False)
+    if args.what == 'single_trial_tms':
+        path = os.path.join(gl.baseDir, gl.behavDir, f'p{args.sn}_testing')
+        dat = pd.read_csv(os.path.join(path, f'efcTMS_testing_{args.sn}_output.csv'))
+        ntrial = dat.shape[0]
+        out_dict = {
+            'participant_id': [],
+            'TN': [],
+            'BN': [],
+            'Chord': [],
+            'condition': [],
+            'session': [],
+            'success': [],
+            'MD': [],
+            'ET': [],
+             **{ch: [] for ch in gl.channels}
+        }
+        force_tms = np.zeros((ntrial, len(gl.channels), 150))
+        for tr in range(ntrial):
+            print(f'Processing trial {tr + 1}...')
+            trial_data = pd.read_csv(os.path.join(path, 'raw_data', f'p{args.sn}_trial{tr + 1}.csv'))
+            force = trial_data[gl.channels].to_numpy() / 1000 # divide by 1000 to get force in N
+            planTime = dat.loc[tr].Prep_dur
+            holdTime = dat.loc[tr].Match_dur
+            Type = dat.loc[tr].Type
+            start_sample = int(np.floor(planTime * gl.sampling_rate))
+            if Type=='Chord':
+                end_sample = force.shape[0] - int(np.floor(holdTime * gl.sampling_rate))
+                success = dat.loc[tr].Success == 'success'
+                X = force[start_sample:end_sample]
+                force_pattern = force[end_sample:].mean(axis=0)
+                MD, d = calc_md(X)
+                ET = (end_sample - start_sample) / gl.sampling_rate
+            elif Type=='TMS':
+                forceAbs = np.abs(force[start_sample:])
+                peakLoc = np.argmax(forceAbs[:50], axis=0) + start_sample
+                force_pattern = force[peakLoc, np.arange(force.shape[1])]
+                force_tms[tr, :, :] = force[:150].T
+                MD = np.nan
+                ET = np.nan
+                success = np.nan
+            for c, ch in enumerate(gl.channels):
+                out_dict[ch].append(force_pattern[c])
+            out_dict['MD'].append(MD)
+            out_dict['ET'].append(ET)
+            out_dict['participant_id'].append(args.sn)
+            out_dict['TN'].append(tr + 1)
+            out_dict['BN'].append(dat.loc[tr].Block)
+            out_dict['Chord'].append(dat.loc[tr].Chord)
+            out_dict['condition'].append(dat.loc[tr].Type)
+            out_dict['session'].append('testing')
+            out_dict['success'].append(success)
+        df = pd.DataFrame(out_dict)
+        df.to_csv(os.path.join(path, 'single_trial.tsv'), sep='\t', index=False)
+        np.save(os.path.join(path, 'forceTMS.npy'), force_tms)
+
+
+
+if __name__ == '__main__':
+
+    start = time.time()
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('what', nargs='?', default=None)
+    parser.add_argument('--sn', type=int, default=100)
+    parser.add_argument('--sns', nargs='+', default=[101], type=int)
+
+    args = parser.parse_args()
+
+    main(args)
+
+    end = time.time()
+    print(f'Time elapsed: {end - start} seconds')
---
+++ globals.py
@@ -0,0 +1,18 @@
+
+
+baseDir = '/cifs/diedrichsen/data/Chord_exp/EFC_learningTMS'
+behavDir = 'behavioural'
+sampling_rate = 100 # sampling rate in Hz
+
+channels = ['finger_1_force_x',	'finger_1_force_y',	'finger_1_force_z',
+'finger_2_force_x',	'finger_2_force_y',	'finger_2_force_z',
+'finger_3_force_x',	'finger_3_force_y',	'finger_3_force_z',
+'finger_4_force_x',	'finger_4_force_y',	'finger_4_force_z',
+'finger_5_force_x',	'finger_5_force_y',	'finger_5_force_z',	]
+
+chord_mapping = {
+    'C1': 0,
+    'C2': 1,
+    'C3': 2,
+    'C4': 3,
+}
